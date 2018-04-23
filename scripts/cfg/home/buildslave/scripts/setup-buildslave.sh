d0ca66831b9997089fe68f2e817a3066ae15eff2
abma <spring@abma.de>
2018-04-24 01:18:37 +0200

    workarround https://github.com/lxc/lxd/issues/2323 by avoiding sudo

--- scripts/cfg/home/buildslave/scripts/setup-buildslave.sh
+++ scripts/cfg/home/buildslave/scripts/setup-buildslave.sh
@@ -3,17 +3,12 @@
 set -e
 source $(dirname $0)/make_static_libs_common.sh
 
-BUILDBOTDIR=/home/buildslave
-
 apt update
 apt -y install g++ make cmake p7zip-full ninja-build \
 	libxmu-dev libxi-dev default-jre default-jdk \
 	libcurl4-gnutls-dev libssl-dev libopenal-dev libvorbis-dev \
 	libogg-dev libsdl2-dev libfreetype6-dev libfontconfig1-dev \
 	freeglut3-dev libgif-dev \
-	buildbot-slave
-
-buildslave create-slave $BUILDBOTDIR localhost:8888 buildslave-x64 changeme
-chown -R buildbot:buildbot $BUILDBOTDIR
-
+	buildbot-slave \
+	pwgen
 
--- scripts/setup-buildslaves.sh
+++ scripts/setup-buildslaves.sh
@@ -10,14 +10,15 @@ for arch in x64 i686; do
 	b=buildslave-$arch
 	echo $b
 	cd $CFG
-	#for i in $(find -type f);
-	#do
-	#	echo lxc file push $i $b/$i
-	#	lxc file push $i $b/$i -p
-	#done
-	#lxc exec $b -- git clone https://github.com/spring/spring-lxc /home/builslave/spring-lxc
+	for i in $(find -type f);
+	do
+		echo lxc file push $i $b/$i
+		lxc file push $i $b/$i -p
+	done
 	lxc exec $b -- /home/buildslave/scripts/setup-buildslave.sh
-	#lxc exec $b -- /home/buildslave/scripts/make_static_libs.sh
+	lxc exec $b -- /home/buildslave/scripts/make_static_libs.sh
+	echo lxc exec $b -- /home/buildslave/scripts/setup-auth.sh
+	lxc exec $b -- /home/buildslave/scripts/setup-auth.sh
 	lxc exec $b -- apt clean
 done
 
