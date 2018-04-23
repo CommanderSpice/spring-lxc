acfabd08f1aa29b572f9efa81af3ae0800e389cb
abma <spring@abma.de>
2018-04-23 21:53:17 +0200

    add .gitignore / code dedup

---
+++ scripts/make_static_libs_common.sh
@@ -0,0 +1,30 @@
+
+set -e
+
+export WORKDIR=${PWD}
+export TMPDIR=${WORKDIR}/tmp
+export INCLUDEDIR=${WORKDIR}/include
+export LIBDIR=${WORKDIR}/lib
+export MAKE="make -j2"
+export DLDIR=${WORKDIR}/download
+
+rm -rf ${TMPDIR}
+rm -rf ${INCLUDEDIR}
+rm -rf ${LIBDIR}
+
+mkdir -p ${TMPDIR}
+mkdir -p ${INCLUDEDIR}
+mkdir -p ${LIBDIR}
+mkdir -p ${DLDIR}
+
+function wget {
+	URL=$1
+	FILENAME=${DLDIR}/$(basename $1)
+	if ! [ -s $FILENAME ]; then
+		/usr/bin/wget $1 -O $FILENAME
+	fi
+
+	cd $(mktemp -d)
+	tar xifzv $FILENAME --strip-components=1
+}
+
