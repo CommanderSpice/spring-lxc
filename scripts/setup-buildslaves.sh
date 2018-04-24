637f75bf37bc9420e4ec9b4d9c1f674209eaa9da
abma <spring@abma.de>
2018-04-24 11:27:35 +0200

    fix path

--- scripts/setup-buildslaves.sh
+++ scripts/setup-buildslaves.sh
@@ -9,7 +9,8 @@ CFG=$SCRIPTPATH/cfg
 for arch in x64 i686; do
 	b=buildslave-$arch
 	echo $b
-	lxc file push $CFG $b/ -p -r
+	cd $CFG
+	lxc file push . $b/ -p -r
 	lxc exec $b -- /home/buildslave/scripts/setup-buildslave.sh
 	lxc exec $b -- /home/buildslave/scripts/make_static_libs.sh
 	lxc exec $b -- /home/buildslave/scripts/setup-auth.sh
