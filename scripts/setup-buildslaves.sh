9b69f7fad3160edfbb8b29fa93fd4381d5414be2
abma <spring@abma.de>
2018-04-24 11:24:47 +0200

    use recursive push

--- scripts/setup-buildslaves.sh
+++ scripts/setup-buildslaves.sh
@@ -9,12 +9,7 @@ CFG=$SCRIPTPATH/cfg
 for arch in x64 i686; do
 	b=buildslave-$arch
 	echo $b
-	cd $CFG
-	for i in $(find -type f);
-	do
-		echo lxc file push $i $b/$i
-		lxc file push $i $b/$i -p
-	done
+	lxc file push $CFG $b/ -p -r
 	lxc exec $b -- /home/buildslave/scripts/setup-buildslave.sh
 	lxc exec $b -- /home/buildslave/scripts/make_static_libs.sh
 	lxc exec $b -- /home/buildslave/scripts/setup-auth.sh
