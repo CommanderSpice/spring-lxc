cc10bb7ca74cdd41e1c67f1456842e4cb36c0c84
abma <spring@abma.de>
2018-04-25 11:30:19 +0200

    fix deps

--- scripts/cfg/home/buildbot/scripts/setup-buildslave.sh
+++ scripts/cfg/home/buildbot/scripts/setup-buildslave.sh
@@ -6,7 +6,7 @@ source $(dirname $0)/make_static_libs_common.sh
 apt update
 apt -y install g++ make cmake p7zip-full ninja-build \
 	libxmu-dev libxi-dev default-jre default-jdk \
-	libcurl4-gnutls-dev libssl-dev libopenal-dev libvorbis-dev \
+	libcurl4-openssl-dev libssl-dev libopenal-dev libvorbis-dev \
 	libogg-dev libsdl2-dev libfreetype6-dev libfontconfig1-dev \
 	freeglut3-dev libgif-dev \
 	buildbot-slave \
