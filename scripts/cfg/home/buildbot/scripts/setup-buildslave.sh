518a9418845b069d24e85395462950791f63a920
abma <spring@abma.de>
2018-04-25 20:20:47 +0200

    removed because of https://github.com/spring/spring/commit/a59beeb5d8c7d248fcb1d5b4af4ddd306c665f78 + https://github.com/spring/spring/commit/5eb1ebf7d846df8cd32e612793c3c018c7712a88

--- scripts/cfg/home/buildbot/scripts/setup-buildslave.sh
+++ scripts/cfg/home/buildbot/scripts/setup-buildslave.sh
@@ -12,6 +12,6 @@ apt -y install g++ make cmake p7zip-full ninja-build \
 	buildbot-slave \
 	pwgen \
 	autossh \
-	libboost-test-dev libboost-chrono-dev libboost-thread-dev libboost-system-dev \
+	libboost-test-dev \
 	chrpath
 
