6379de2d747fc02cb071f5e09cc520eb5d668a62
abma <spring@abma.de>
2018-05-02 17:48:39 +0200

    keep existing mxe install

--- scripts/cfg/install/install-mxe.sh
+++ scripts/cfg/install/install-mxe.sh
@@ -6,6 +6,11 @@ cd $1
 
 COMMIT=0dcf498f9563fd35eff0273e1b56929fdce60383
 
+if [ -x mxe/usr/bin/i686-w64-mingw32.static.posix-cmake ]; then
+	echo mxe g++ already exists, skipping!
+	exit 0
+fi
+
 if [ ! -d mxe ]; then
 	git clone https://github.com/mxe/mxe.git
 	cd mxe
@@ -14,6 +19,7 @@ else
 	git fetch origin
 	git clean -f -d -x
 fi
+
 
 git reset --hard $COMMIT
 
