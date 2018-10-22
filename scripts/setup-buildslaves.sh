67f031638c1da6089ab949f93108185d570c50c6
abma <spring@abma.de>
2018-10-23 00:09:12 +0200

    convert from lxd to lxc

--- scripts/setup-buildslaves.sh
+++ scripts/setup-buildslaves.sh
@@ -9,27 +9,36 @@ LOCAL=$SCRIPTPATH/local
 ARCHITECTURES="x64 i686 win32"
 #ARCHITECTURES="win32"
 
+CONTAINERDIR=$(lxc-config lxc.lxcpath)
+if [ -n "$CONTAINERDIR" ] || ! [ -d "$CONTAINERDIR" ]; then
+	echo "lxc.lxcpath '$CONTAINERDIR' doesn't exist!"
+	exit 1
+fi
+
 for arch in $ARCHITECTURES; do
 	b=buildslave-$arch
 	echo $b
-	cd $CFG
-	lxc file push . $b/ -p -r -v
-	cd $LOCAL
-	lxc file push . $b/ -p -r
+	CONTAINERROOT=$CONTAINERDIR/$b/rootfs
+	if ! [ -d "$CONTAINERROOT"]; then
+		echo "rootfs '$CONTAINERROOT' doesn't exist!"
+		exit 1
+	fi
+	rsync -av "$CFG"/ "$CONTAINERROOT/"
+	rsync -av "$LOCAL"/ "$CONTAINERROOT"/
 
 	case $arch in
 		"x64")
 		;;
 		"i686")
-			lxc exec $b -- /install/install-linux.sh
-			lxc exec $b -- /install/make_static_libs.sh /home/buildbot/lib
-			lxc exec $b -- /install/setup-auth.sh
+			lxc-attach --name $b -- /install/install-linux.sh
+			lxc-attach --name $b -- /install/make_static_libs.sh /home/buildbot/lib
+			lxc-attach --name $b -- /install/setup-auth.sh
 		break
 		;;
 		"win32")
-			lxc exec $b -- /install/install-win32.sh
-			lxc exec $b -- /install/install-mxe.sh /home/buildbot
-			lxc exec $b -- /install/setup-auth.sh
+			lxc-attach --name $b -- /install/install-win32.sh
+			lxc-attach --name $b -- /install/install-mxe.sh /home/buildbot
+			lxc-attach --name $b -- /install/setup-auth.sh
 		break
 		;;
 		*)
@@ -39,11 +48,11 @@ for arch in $ARCHITECTURES; do
 		;;
 	esac
 	
-	lxc exec $b -- apt clean
-	lxc exec $b -- rm -rf /home/buildbot/lib/tmp /home/buildbot/lib/download /install
-	lxc exec $b -- systemctl daemon-reload
-	lxc exec $b -- systemctl start autossh
-	lxc exec $b -- systemctl start buildslave
-	lxc exec $b -- chown -R buildbot:buildbot /home/buildbot
+	lxc-attach --name $b -- apt clean
+	lxc-attach --name $b -- rm -rf /home/buildbot/lib/tmp /home/buildbot/lib/download /install
+	lxc-attach --name $b -- systemctl daemon-reload
+	lxc-attach --name $b -- systemctl start autossh
+	lxc-attach --name $b -- systemctl start buildslave
+	lxc-attach --name $b -- chown -R buildbot:buildbot /home/buildbot
 done
 
